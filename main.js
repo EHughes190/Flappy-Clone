5100462a872d630b613803b1879cc0a1afcd5ed7
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2021-05-25 22:16:55 +0100

    Added a modal for game over, as well as game over logic

--- main.js
+++ main.js
@@ -19,11 +19,16 @@
 //Particle effects
 
 const bird = document.querySelector(".bird");
-const scoreText = document.querySelector(".score_text");
+const scoreDiv = document.querySelector(".score");
+const scoreText = document.querySelector(".score__text");
+const finalScore = document.getElementById("final-score");
 const gameContainer = document.querySelector(".game-container");
 const startButton = document.querySelector(".button");
+const menuButton = document.querySelector(".menu-btn");
+const heading = document.querySelector(".heading");
 const hole = document.querySelector(".hole");
 const pipe = document.querySelector(".pipe");
+const modalBg = document.querySelector(".modal-bg");
 
 let gravity = 5;
 let pipeGapX = 0;
@@ -34,20 +39,46 @@ let jumping = 0;
 let executed = false;
 let score = 0;
 let gameOver = false;
+let gravityAnim;
 
 startButton.addEventListener("click", startGame);
 
+menuButton.addEventListener("click", reset);
+
+function reset() {
+  modalBg.classList.remove("bg-active");
+  hole.classList.remove("animated");
+  pipe.classList.remove("animated");
+  startButton.style.display = "unset";
+  heading.style.display = "unset";
+  scoreDiv.classList.remove("visible");
+  gameOver = false;
+}
+
+function endGame() {
+  modalBg.classList.add("bg-active");
+  cancelAnimationFrame(gravityAnim);
+  gameOver = true;
+  finalScore.innerHTML = scoreText.innerHTML;
+  //pipe.style.webkitAnimationPlayState = "paused";
+  //hole.style.webkitAnimationPlayState = "paused";
+}
+
 function startGame() {
+  scoreDiv.classList.add("visible");
   startButton.style.display = "none";
+  heading.style.display = "none";
   scoreText.innerHTML = score;
   hole.classList.add("animated");
   pipe.classList.add("animated");
 
   hole.addEventListener("animationiteration", () => {
-    let randomHeight = -(Math.random() * (100 - 50) + 30);
-    hole.style.top = randomHeight + "vh";
-    score++;
-    scoreText.innerHTML = score;
+    if (!gameOver) {
+      let randomHeight = -(Math.random() * (100 - 50) + 30);
+      hole.style.top = randomHeight + "vh";
+      score++;
+      scoreText.innerHTML = score;
+    }
   });
 
   applyGravity();
@@ -63,143 +94,137 @@ function startGame() {
   document.addEventListener("keyup", () => {
     fired = false;
   });
-
-  function fly() {
-    jumping = 1;
-    let jumpCount = 0;
-    let jumpInterval = setInterval(function () {
-      birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
-      if (birdTop >= 90 && jumpCount < 17) {
-        bird.style.top = birdTop - gravity + "px";
-      }
-      if (jumpCount > 20) {
-        clearInterval(jumpInterval);
-        jumping = 0;
-        jumpCount = 0;
-      }
-      jumpCount++;
-    }, 10);
-  }
-
-  function applyGravity() {
-    let birdTop = parseInt(
-      window.getComputedStyle(bird).getPropertyValue("top")
-    );
-    let birdBot = parseInt(
-      window.getComputedStyle(bird).getPropertyValue("bottom")
-    );
-    //if jumping is pressed, gravity isn't allowed to operate
-    if (jumping === 0) {
-      bird.style.top = birdTop + gravity * 1.2 + "px";
-    }
-
-    if (birdBot <= 0) {
-      gameOver();
-    }
-
-    requestAnimationFrame(applyGravity);
-  }
-
-  function checkCollisions() {
-    let holeRect = hole.getBoundingClientRect();
-    let pipeRect = pipe.getBoundingClientRect();
-    let birdRect = bird.getBoundingClientRect();
-    let holeHeight = parseInt(
-      window.getComputedStyle(hole).getPropertyValue("height")
-    );
-    let birdHeight = parseInt(
-      window.getComputedStyle(bird).getPropertyValue("height")
-    );
-
-    if (
-      birdRect.left <= pipeRect.right &&
-      birdRect.right >= pipeRect.left &&
-      (birdTop < holeRect.top ||
-        birdTop > holeRect.top + (holeHeight - birdHeight))
-    ) {
-      gameOver();
-    }
-    requestAnimationFrame(checkCollisions);
-  }
-
-  function gameOver() {
-    gameOver = true;
-  }
-
-  // function generatePipe() {
-  //   if (pipeGapX > 100) {
-  //     pipeGapX = 0;
-  //     let pipeLeft = 110;
-  //     let pipeHole = 30;
-  //     //Math.random() * (max - min) + min; gives a number within the bounds we set as max and min
-  //     let randomHeight = Math.random() * (60 - 90) + 90;
-
-  //     //Creating pipe on ground
-  //     const pipe = document.createElement("div");
-  //     pipe.style.top = randomHeight + "vh";
-  //     pipe.classList.add("pipe");
-  //     gameContainer.appendChild(pipe);
-  //     pipe.style.left = pipeLeft + "vw";
-
-  //     //Creating pipe on ceiling
-  //     const pipe_inverse = document.createElement("div");
-  //     pipe_inverse.classList.add("pipe");
-  //     pipe_inverse.style.top = randomHeight - pipeHole - 70 + "vh";
-  //     gameContainer.appendChild(pipe_inverse);
-  //     pipe_inverse.style.left = pipeLeft + "vw";
-
-  //     //MOVE PIPES FROM RIGHT TO LEFT, DESPAWNING THEM IF OUT OF CONTAINER
-  //     function movePipe() {
-  //       pipeLeft -= 2;
-  //       pipe.style.left = pipeLeft + "vw";
-  //       pipe_inverse.style.left = pipeLeft + "vw";
-
-  //       if (pipe.style.left <= -10 + "vw") {
-  //         pipe.remove();
-  //         pipe_inverse.remove();
-  //       } else {
-  //COLLISION LOGIC
-  //   let pipe_rect = pipe.getBoundingClientRect();
-  //   let pipe_inverse_rect = pipe_inverse.getBoundingClientRect();
-  //   let bird_rect = bird.getBoundingClientRect();
-
-  //   if (pipe_rect.right < bird_rect.left ) {
-  //     pipe
-  //   }
-
-  //   if (
-  //     (bird_rect.left <= pipe_rect.right &&
-  //       bird_rect.right > pipe_rect.left &&
-  //       bird_rect.top <= pipe_rect.bottom &&
-  //       bird_rect.bottom >= pipe_rect.top) ||
-  //     (bird_rect.left <= pipe_inverse_rect.right &&
-  //       bird_rect.right > pipe_inverse_rect.left &&
-  //       bird_rect.top <= pipe_inverse_rect.bottom &&
-  //       bird_rect.bottom >= pipe_inverse_rect.top)
-  //   ) {
-  //     alert("Game over");
-  //   } else
-  // }
-  //     }
-  //     setInterval(movePipe, 40);
-  //   }
-
-  //   pipeGapX++;
-  //   requestAnimationFrame(generatePipe);
-  // }
-  // generatePipe();
-  //}
-
-  // function gameOver() {
-  //   console.log("Dead");
-  // }
-
-  // function updateScore() {
-  //   if (!executed) {
-  //     executed = true;
-  //     // do something
-  //     score++;
-  //     console.log(score);
-  //     scoreText.innerHTML = score;
-  //   }
 }
+
+function fly() {
+  jumping = 1;
+  let jumpCount = 0;
+  let jumpInterval = setInterval(function () {
+    birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+    if (birdTop >= 90 && jumpCount < 17) {
+      bird.style.top = birdTop - gravity + "px";
+    }
+    if (jumpCount > 20) {
+      clearInterval(jumpInterval);
+      jumping = 0;
+      jumpCount = 0;
+    }
+    jumpCount++;
+  }, 10);
+}
+
+function applyGravity() {
+  let birdTop = parseInt(window.getComputedStyle(bird).getPropertyValue("top"));
+  let birdBot = parseInt(
+    window.getComputedStyle(bird).getPropertyValue("bottom")
+  );
+  //if jumping is pressed, gravity isn't allowed to operate
+  if (jumping === 0) {
+    bird.style.top = birdTop + gravity * 1.2 + "px";
+  }
+
+  if (birdBot <= 0) {
+    endGame();
+  }
+
+  gravityAnim = requestAnimationFrame(applyGravity);
+}
+
+function checkCollisions() {
+  let holeRect = hole.getBoundingClientRect();
+  let pipeRect = pipe.getBoundingClientRect();
+  let birdRect = bird.getBoundingClientRect();
+  let holeHeight = parseInt(
+    window.getComputedStyle(hole).getPropertyValue("height")
+  );
+  let birdHeight = parseInt(
+    window.getComputedStyle(bird).getPropertyValue("height")
+  );
+
+  if (
+    birdRect.left < pipeRect.right &&
+    birdRect.right > pipeRect.left &&
+    (birdTop < holeRect.top ||
+      birdTop > holeRect.top + (holeHeight - birdHeight))
+  ) {
+    endGame();
+  }
+  requestAnimationFrame(checkCollisions);
+}
+
+// function generatePipe() {
+//   if (pipeGapX > 100) {
+//     pipeGapX = 0;
+//     let pipeLeft = 110;
+//     let pipeHole = 30;
+//     //Math.random() * (max - min) + min; gives a number within the bounds we set as max and min
+//     let randomHeight = Math.random() * (60 - 90) + 90;
+
+//     //Creating pipe on ground
+//     const pipe = document.createElement("div");
+//     pipe.style.top = randomHeight + "vh";
+//     pipe.classList.add("pipe");
+//     gameContainer.appendChild(pipe);
+//     pipe.style.left = pipeLeft + "vw";
+
+//     //Creating pipe on ceiling
+//     const pipe_inverse = document.createElement("div");
+//     pipe_inverse.classList.add("pipe");
+//     pipe_inverse.style.top = randomHeight - pipeHole - 70 + "vh";
+//     gameContainer.appendChild(pipe_inverse);
+//     pipe_inverse.style.left = pipeLeft + "vw";
+
+//     //MOVE PIPES FROM RIGHT TO LEFT, DESPAWNING THEM IF OUT OF CONTAINER
+//     function movePipe() {
+//       pipeLeft -= 2;
+//       pipe.style.left = pipeLeft + "vw";
+//       pipe_inverse.style.left = pipeLeft + "vw";
+
+//       if (pipe.style.left <= -10 + "vw") {
+//         pipe.remove();
+//         pipe_inverse.remove();
+//       } else {
+//COLLISION LOGIC
+//   let pipe_rect = pipe.getBoundingClientRect();
+//   let pipe_inverse_rect = pipe_inverse.getBoundingClientRect();
+//   let bird_rect = bird.getBoundingClientRect();
+
+//   if (pipe_rect.right < bird_rect.left ) {
+//     pipe
+//   }
+
+//   if (
+//     (bird_rect.left <= pipe_rect.right &&
+//       bird_rect.right > pipe_rect.left &&
+//       bird_rect.top <= pipe_rect.bottom &&
+//       bird_rect.bottom >= pipe_rect.top) ||
+//     (bird_rect.left <= pipe_inverse_rect.right &&
+//       bird_rect.right > pipe_inverse_rect.left &&
+//       bird_rect.top <= pipe_inverse_rect.bottom &&
+//       bird_rect.bottom >= pipe_inverse_rect.top)
+//   ) {
+//     alert("Game over");
+//   } else
+// }
+//     }
+//     setInterval(movePipe, 40);
+//   }
+
+//   pipeGapX++;
+//   requestAnimationFrame(generatePipe);
+// }
+// generatePipe();
+//}
+
+// function gameOver() {
+//   console.log("Dead");
+// }
+
+// function updateScore() {
+//   if (!executed) {
+//     executed = true;
+//     // do something
+//     score++;
+//     console.log(score);
+//     scoreText.innerHTML = score;
+//   }
