afae1bbea4c7bf193f9c3ca304648d9e41c62f84
Ed Hughes <77494320+EHughes190@users.noreply.github.com>
2021-05-27 22:00:27 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -87,7 +87,7 @@ function startGame() {
   scoreText.innerHTML = 0;
   hole.classList.add("animated");
   pipe.classList.add("animated");
-  highscore = parseInt(localStorage.getItem("highscore"));
+  highscore = parseInt(localStorage.getItem("highscore"), 10);
 
   if (!gameOver) {
     applyGravity();
